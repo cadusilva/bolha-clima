028f6df2a7b0bf63639a902dbfaa2b41245c240e
Cadu Silva <contato@cadusilva.com>
2024-01-12 00:14:41 -0300

    Voltando a usar API OpenWeatherMap

--- openweathermap.py
+++ openweathermap.py
@@ -30,20 +30,20 @@ import urllib.request
 from dotenv import load_dotenv
 from decimal import Decimal
 
-BASE_URL = "https://api.weatherapi.com/v1/current.json?q="
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q="
 executor = concurrent.futures.ThreadPoolExecutor()
 logger = logging.getLogger(__name__)
 
 
-def try_city(city_name, api_key: str, lang="pt", timeout: int = None) -> typing.Union[str, int]:
+def try_city(city_name, api_key: str, lang="pt_br", timeout: int = None) -> typing.Union[str, int]:
     city_name = city_name.strip().rstrip("!?").replace("&apos;", "'").strip()
 
     full_api_url = (
         BASE_URL
         + urllib.parse.quote(city_name)
-        + "&aqi=no&lang="
+        + "&units=metric&lang="
         + lang
-        + "&key="
+        + "&appid="
         + api_key
     )
 
@@ -57,27 +57,23 @@ def try_city(city_name, api_key: str, lang="pt", timeout: int = None) -> typing.
         logger.exception("Algo inesperado aconteceu.")
         return 500
 
-    updated     = json_data.get("current").get("last_updated")[-5:]
-    city        = json_data.get("location").get("name")
-    region      = json_data.get("location").get("region")
-    country     = json_data.get("location").get("country")
-    weather     = json_data.get("current").get("condition").get("text").lower()
-    temp        = json_data.get("current").get("temp_c")
-    feelslike   = json_data.get("current").get("feelslike_c")
-    humidity    = json_data.get("current").get("humidity")
-    clouds      = json_data.get("current").get("cloud")
-    uv          = json_data.get("current").get("uv")
+    city        = json_data.get("sys").get("name")
+    country     = json_data.get("sys").get("country")
+    weather     = json_data.get("weather")[0].get("description").lower()
+    temp        = json_data.get("main").get("temp")
+    feelslike   = json_data.get("main").get("feels_like")
+    humidity    = json_data.get("main").get("humidity")
+    clouds      = json_data.get("clouds").get("all")
 
-    lat         = json_data.get("location").get("lat")
-    lon         = json_data.get("location").get("lon")
+    lat         = json_data.get("coord").get("lat")
+    lon         = json_data.get("coord").get("lon")
 
     i_temp      = round(Decimal(temp))
     i_feelslike = round(Decimal(feelslike))
-    i_uv        = round(Decimal(uv))
-#    i_humidity  = "{:.0f}".format(humidity)
+    i_humidity  = "{:.0f}".format(humidity)
 #    i_clouds    = "{:.0f}".format(clouds)
 
-    return f"Esse é o clima atual em {city} ({region}, {country}):\n\n:temp: Temperatura: {i_temp} \xb0C\n:s_termica: Sensação térmica: {i_feelslike} \xb0C\n:ceu: Céu agora: {weather}, {clouds}% encoberto\n:umidade: Umidade do ar: {humidity}%\n:sunny: Índice UV: {i_uv} de 11\n\n\U0001f552 Atualizado às {updated} (horário local)\n\U0001f5fa\uFE0F Ver cidade no mapa: https://www.openstreetmap.org/?mlat={lat}&mlon={lon}\n\u2139\uFE0F Com informações de WeatherAPI\n\n#clima #BolhaClima"
+    return f"Esse é o clima atual em {city} ({country}):\n\n:temp: Temperatura: {i_temp} \xb0C\n:s_termica: Sensação térmica: {i_feelslike} \xb0C\n:ceu: Céu agora: {weather}, {clouds}% encoberto\n:umidade: Umidade do ar: {i_humidity}%\n\n\U0001f5fa\uFE0F Ver cidade no mapa: https://www.openstreetmap.org/?mlat={lat}&mlon={lon}\n\u2139\uFE0F Com informações de OpenWeatherMap\n\n#clima #BolhaClima"
 
 
 def _read_json(url):
